23f77cdb2a7581e62c9ad875692b282ee5938bea
marlonbetz <marlon.betz@gmx.de>
2016-08-12 13:19:28 +0200

    added clustering

--- Workspace/Workspace/plotters/vae_phono/plotter.py
+++ Workspace/Workspace/plotters/vae_phono/plotter.py
@@ -20,24 +20,45 @@ if embeddings.shape[1] != 2:
     embeddings_transformed = tsne.fit_transform(embeddings)
 else:
     embeddings_transformed = embeddings
-    
+print("clustering")
+c2c = [5,6,7,8]
+labels = dict()
+from sklearn.cluster import AffinityPropagation
+ap = AffinityPropagation()
+for c in c2c:
+    labels[c] = ap.fit_predict([emb for emb,concept 
+                                in zip(embeddings_transformed,concepts) if concept ==c])
+print(labels)
 print("plotting")
 import matplotlib.pyplot as plt
 import seaborn
 cmap = dict((key,np.random.beta(1,1,3)) for key in cognate_classes)
+counters = {5:0,6:0,7:0,8:0}
 for asjp_word,emb,cognate_class,concept in zip(asjp_words,embeddings_transformed,cognate_classes,concepts):
 #     plt.annotate(asjp_word,emb,color=cmap[cognate_class])
     if concept == 5:
         plt.subplot(2,2,1)
-        plt.annotate(asjp_word,emb,color=cmap[cognate_class])
+        label = labels[5][counters[5]]
+        plt.annotate(asjp_word+"_"+str(label),emb,color=cmap[cognate_class])
+        counters[5] += 1
+        
     if concept == 6:
         plt.subplot(2,2,2)
-        plt.annotate(asjp_word,emb,color=cmap[cognate_class])
+        label = labels[6][counters[6]]
+        plt.annotate(asjp_word+"_"+str(label),emb,color=cmap[cognate_class])
+        counters[6] += 1
+
     if concept == 7:
         plt.subplot(2,2,3)
-        plt.annotate(asjp_word,emb,color=cmap[cognate_class])
+        label = labels[7][counters[7]]
+        plt.annotate(asjp_word+"_"+str(label),emb,color=cmap[cognate_class])
+        counters[7] += 1
+
     if concept == 8:
         plt.subplot(2,2,4)
-        plt.annotate(asjp_word,emb,color=cmap[cognate_class])
+        label = labels[8][counters[8]]
+        plt.annotate(asjp_word+"_"+str(label),emb,color=cmap[cognate_class])
+        counters[8] += 1
+        
 
 plt.show()
